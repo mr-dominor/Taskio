c549ed989a6d92f8f167894753962155d1b37c8c
mubashshir <mubashshirislam201@gmail.com>
2025-12-11 15:32:57 +0530

    The Taskio on ground***

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -1,37 +1,45 @@
 "use server"
-import { NextResponse,NextRequest } from "next/server";
+
+import { NextResponse, NextRequest } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/db";
-export async function GET(req:NextRequest, request: NextRequest, context: any){
-    try {
-        const {userId, orgId} = await auth();
-        const params = context.params
-        const {cardId} = params.cardId
-        console.log("🤖🤖🤖 CardId::",cardId)
-        if(!userId || !orgId) {
-            return new NextResponse("Unauthorized",{status:401})
-        }
 
-        const card = await prisma.card.findUnique({
-            where:{
-                id:cardId,
-                list:{
-                    board:{
-                        orgId,
-                    },
-                },
-            },
-            include:{
-                list:{
-                    select:{
-                        title:true,
-                    },
-                },
-            },
-        });
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { cardId: string } }
+) {
+  try {
+    const { userId, orgId } = await auth();
 
-        return NextResponse.json(card)
-    } catch (error) {
-        return new NextResponse("Internal Error",{status:500})
+    if (!userId || !orgId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const { cardId } = params;
+
+    console.log("🤖🤖🤖 CardId::", cardId);
+
+    const card = await prisma.card.findUnique({
+      where: {
+        id: cardId,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+      include: {
+        list: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(card);
+  } catch (error) {
+    console.error("GET /api/cards/[cardId] error:", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
